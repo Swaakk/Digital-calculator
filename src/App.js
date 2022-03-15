54997dfa21d52c4670cc4466004d6ac1fcd4eae2
Swaakk <swathykrishna.thayyil@gmail.com>
2022-03-15 23:02:53 +0530

    started functionality

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 function App() {
   const time = new Date().toLocaleTimeString();
   const[ctime,seyCtime]=useState(time)
+
+  const UpdateTime=()=>{
+    time = new Date().toLocaleTimeString();
+    seyCtime(time)
+  }
   return (
     <div className="time-body">
      <h1>{ctime}</h1>
+     <button className="btn" onClick={UpdateTime}>Get Time</button>
     </div>
   );
 }
