70ed34af3a088d0b147e75d8c02a3e87624ac3ce
Swaakk <swathykrishna.thayyil@gmail.com>
2022-03-15 22:43:37 +0530

    started jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import react,{useState} from "react"
 import './App.css';
 
 function App() {
+  const time = new Date().toLocaleTimeString();
+  const[ctime,seyCtime]=useState(time)
   return (
-    <div className="App">
-     
+    <div >
+     <h1>{ctime}</h1>
     </div>
   );
 }
